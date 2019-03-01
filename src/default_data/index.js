62580dbc9dcec66a14077378ba29effcdfad2d3d
Kramerbaf <nds.kram@gmail.com>
2019-03-01 14:32:23 +0100

    changed langs

--- src/default_data/index.js
+++ src/default_data/index.js
@@ -2,10 +2,10 @@ module.exports = [
     {
         name: 'headband_A',
         storeName: {
-            en_GB: "headband_A"
+            en: "headband_A"
         },
         storeDesc: {
-            en_GB: ""
+            en: ""
         }
     }
 ]
